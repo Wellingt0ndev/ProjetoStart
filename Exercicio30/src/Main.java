010107df6652b04ddc9b17a9864267707fd45925
Wellingt0ndev <wellington_006@hotmail.com>
2022-09-27 17:19:34 -0300

    Add uma nova resolução

--- Exercicio30/src/Main.java
+++ Exercicio30/src/Main.java
@@ -4,8 +4,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //Escreva um algoritmo que leia três valores inteiros distintos e os escreva em ordem crescente;
-        int valor1, valor2, valor3;
+        int[] valores = new int[3];
+        int aux;
         Scanner leitor = new Scanner(System.in);
+        //Carregando os números no vetor
+        for (int i = 0; i <= 2; i++) {
+            System.out.println("Digite o " + (i + 1) + "º número: ");
+            valores[i] = leitor.nextInt();
+        }
+        //Ordenando de forma crescente
+        //Laço com a quantidade de elementos do vetor - 1
+        for (int j = 1; j <= 2; j++) {
+            //Laço percorre da última posição à posição j do vetor
+            for (int i = 2; i >= j; i--) {
+                if (valores[i] < valores[i - 1]) {
+                    aux = valores[i];
+                    valores[i] = valores[i - 1];
+                    valores[i - 1] = aux;
+                }
+            }
+        }
+        //Mostrando o vetor ordenado
+        for (int i = 0; i <= 2; i++) {
+            System.out.printf(valores[i] + " ");
+        }
+    }
+}
+
+
+//SEGUNDA OPÇÃO!
+        /*Scanner leitor = new Scanner(System.in);
+        int valor1, valor2, valor3;
         System.out.println("Digite o primeiro número: ");
         valor1 = leitor.nextInt();
         System.out.println("Digite o segundo número: ");
@@ -13,8 +42,6 @@ public class Main {
         System.out.println("Digite o terceiro número: ");
         valor3 = leitor.nextInt();
         System.out.println(ordenar(valor1, valor2, valor3));
-
-
     }
 
     public static String ordenar(int n1, int n2, int n3) {
@@ -41,4 +68,4 @@ public class Main {
         return n1 + " " + n2 + " " + n3;
     }
 
-}
+}*/
